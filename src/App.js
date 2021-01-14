4af90041bdd0dc207b6eac4cbd4a3473e9893845
Navezm <navez.martin@gmail.com>
2021-01-14 09:31:23 +0100

    Exo finished

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Profil from './Profil';
+
+let App = () => {
+    return(
+        <div>
+            <Profil name="Navez" surname="Martin">Web Développeur</Profil>
+            <Profil name="Primo" surname="Nicolas">Coach</Profil>
+            <Profil name="Bohbot" surname="Marcelin">Animateur Code&Débat</Profil>
+            <Profil name="Ouassari" surname="Ibrahim"></Profil>
+        </div>
+    );
+};
+
+export default App;
